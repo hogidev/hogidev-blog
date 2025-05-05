b58abdc43c508361d8051d568e8fdf6de10593e8
GiangTH <hoanggiang521999@gmail.com>
2025-05-05 23:53:51 +0700

    update style

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -23,38 +23,27 @@ export default function CustomMDX({ content }: { content: string }) {
             return <div {...props}>{children}</div>; // Use a <div> for <pre> blocks to prevent <p> wrapping
           },
           "code"({ className, children }) {
-            const language = className?.replace('language-', '') || '';
             const code = String(children).trim();
 
             return (
-              <div className="relative border border-gray-200/50 rounded-md">
-                <div className={`text-gray-100 flex items-center py-2 px-4 border-b border-gray-200/50 text-xs rounded-t-md ${language ? 'justify-between' : 'justify-end'}`} style={{ backgroundColor: '#2f2f2f' }}>
-                  {/* Language Label */}
-                  {language && (
-                    <span>
-                      {language}
-                    </span>
-                  )}
-
-                  {/* Copy Button */}
-                  <button
-                    onClick={() => handleCopy(code)}
-                    className="flex gap-1 items-center select-none px-3 py-0.5"
-                  >
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ height: '10px', width: '10px' }}>
-                      <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
-                        d="M7 5C7 3.34315 8.34315 2 10 2H19C20.6569 2 22 3.34315 22 5V14C22 15.6569 20.6569 17 19 17H17V19C17 20.6569 15.6569 22 14 22H5C3.34315 22 2 20.6569 2 19V10C2 8.34315 3.34315 7 5 7H7V5ZM9 7H14C15.6569 7 17 8.34315 17 10V15H19C19.5523 15 20 14.5523 20 14V5C20 4.44772 19.5523 4 19 4H10C9.44772 4 9 4.44772 9 5V7ZM5 9C4.44772 9 4 9.44772 4 10V19C4 19.5523 4.44772 20 5 20H14C14.5523 20 15 19.5523 15 19V10C15 9.44772 14.5523 9 14 9H5Z"
-                        fill="currentColor"
-                      ></path>
-                    </svg>
-                    {copiedCode === code ? 'Copied!' : 'Copy'}
-                  </button>
-                </div>
-
+              <div className="code-block-wrapper relative border border-gray-200/50 rounded-md">
+                <button
+                  title="Copy to clipboard"
+                  onClick={() => handleCopy(code)}
+                  className="copy-button p-1 rounded-md bg-zinc-600/50 text-gray-200 text-xs flex gap-1"
+                >
+                  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ height: '16px', width: '16px' }}>
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M7 5C7 3.34315 8.34315 2 10 2H19C20.6569 2 22 3.34315 22 5V14C22 15.6569 20.6569 17 19 17H17V19C17 20.6569 15.6569 22 14 22H5C3.34315 22 2 20.6569 2 19V10C2 8.34315 3.34315 7 5 7H7V5ZM9 7H14C15.6569 7 17 8.34315 17 10V15H19C19.5523 15 20 14.5523 20 14V5C20 4.44772 19.5523 4 19 4H10C9.44772 4 9 4.44772 9 5V7ZM5 9C4.44772 9 4 9.44772 4 10V19C4 19.5523 4.44772 20 5 20H14C14.5523 20 15 19.5523 15 19V10C15 9.44772 14.5523 9 14 9H5Z"
+                      fill="currentColor"
+                    ></path>
+                  </svg>
+                  {copiedCode && 'Copied!'}
+                </button>
                 {/* Code Block */}
-                <pre className="text-gray-100 overflow-x-auto text-sm m-0 p-4 rounded-b-md rounded-t-none" style={{backgroundColor: '#24292e'}}>
+                <pre className="text-gray-100 text-sm m-0 p-4 rounded-md" style={{backgroundColor: '#24292e'}}>
                   <code className={className}>{code}</code>
                 </pre>
               </div>
--- components/scroll-to-top.tsx
+++ components/scroll-to-top.tsx
@@ -31,8 +31,8 @@ export default function ScrollToTopButton() {
         }`}
         aria-label="Scroll to top"
       >
-        <svg className="w-4 fill-zinc-700 stroke-zinc-500" xmlns="http://www.w3.org/2000/svg" fill="none"
-             viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor">
+        <svg className="w-4 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400" xmlns="http://www.w3.org/2000/svg" fill="none"
+             viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18"></path>
         </svg>
       </button>
